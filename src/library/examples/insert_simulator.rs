94dd122f6c63032b491ceb69455f1eec6f9b118f
Christopher Foo <chris.foo@gmail.com>
2021-03-26 20:41:46 -0400

    Add delay argument to examples

--- src/library/examples/insert_simulator.rs
+++ src/library/examples/insert_simulator.rs
@@ -2,11 +2,25 @@
 
 use std::time::{Duration, SystemTime};
 
+use clap::{App, Arg};
 use grebedb::{Database, DatabaseOptions};
 use rand::{RngCore, SeedableRng};
 use rand_xorshift::XorShiftRng;
 
 fn main() -> Result<(), grebedb::Error> {
+    let matches = App::new("GrebeDB insert simulator")
+        .arg(
+            Arg::with_name("delay")
+                .long("delay")
+                .takes_value(true)
+                .default_value("0.5")
+                .help("Delay between insert batches in seconds."),
+        )
+        .get_matches();
+
+    let delay = matches.value_of("delay").unwrap();
+    let delay = delay.parse::<f32>().unwrap();
+
     let path = std::path::PathBuf::from("grebedb_example_data/insert_simulator/");
 
     std::fs::create_dir_all(&path)?;
@@ -33,6 +47,8 @@ fn main() -> Result<(), grebedb::Error> {
             counter += 1;
         }
 
-        std::thread::sleep(Duration::from_secs_f32(0.5))
+        if delay > 0.0 {
+            std::thread::sleep(Duration::from_secs_f32(delay))
+        }
     }
 }
--- src/library/examples/queue_simulator.rs
+++ src/library/examples/queue_simulator.rs
@@ -2,11 +2,25 @@
 
 use std::time::{Duration, SystemTime};
 
+use clap::{App, Arg};
 use rand::{Rng, RngCore, SeedableRng};
 use rand_xorshift::XorShiftRng;
 
 
 fn main() -> Result<(), grebedb::Error> {
+    let matches = App::new("GrebeDB insert simulator")
+        .arg(
+            Arg::with_name("delay")
+                .long("delay")
+                .takes_value(true)
+                .default_value("0.5")
+                .help("Delay between insert/remove batches in seconds."),
+        )
+        .get_matches();
+
+    let delay = matches.value_of("delay").unwrap();
+    let delay = delay.parse::<f32>().unwrap();
+
     let path = std::path::PathBuf::from("grebedb_example_data/queue_simulator/");
 
     std::fs::create_dir_all(&path)?;
@@ -48,6 +62,8 @@ fn main() -> Result<(), grebedb::Error> {
             db.remove(key)?;
         }
 
-        std::thread::sleep(Duration::from_secs_f32(0.5))
+        if delay > 0.0 {
+            std::thread::sleep(Duration::from_secs_f32(delay))
+        }
     }
 }
